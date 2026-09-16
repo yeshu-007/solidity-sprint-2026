8c552ef59db20c576144fd947c189fddab2e382c
R. Yeshwanth <yeshwanthchettyr2006@gmail.com>
2026-09-16 14:59:05 +0530

    session-1 code file upload

--- session-01/contracts/SimpleStorage.sol
+++ session-01/contracts/SimpleStorage.sol
@@ -1,2 +1,32 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT License
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+
+contract Simple_Storage {
+
+    string public message;
+    address public lastEditor;
+
+    
+    //update the message and store the address of the last editor
+    function updateMessage(string memory _newMessage) public {
+        message = _newMessage;
+        lastEditor = msg.sender;
+    }
+
+    //reads the message last sent
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    //fetches the address of the last message editor
+    function getLastEditor() public view returns (address) {
+        return lastEditor;
+    }
+}
